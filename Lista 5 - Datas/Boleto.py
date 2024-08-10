b261c0b3a528915ac1bfec77737b0bf50faf5bf2
IGOR60mog <161069220+IGOR60mog@users.noreply.github.com>
2024-08-10 03:49:45 +0000

    maroon

--- Lista 5 - Datas/Boleto.py
+++ Lista 5 - Datas/Boleto.py
@@ -1,11 +1,36 @@
 from datetime import datetime
 from datetime import timedelta
 
+import enum
+
+class Pagamento(enum.Enum):
+    EmAberto = 1
+    PagoPartical = 2
+    Pago = 3
+
 class Boleto:
 
-    def __init__(self):
-        self.__barras = ""
-        self.__emissao = datetime(1, 1, 1)
-        self.__vencimento = datetime(1, 1, 1)
-        self.__Pagto = datetime(1, 1, 1)
+    def __init__(self, b, e, v, p, vb, vp ):
+        self.__barras = b
+        self.__emissao = e
+        self.__vencimento = v
+        self.__Pagto = p
+        self.__valorBoleto = vb
+        self.__valorPago = vp
+        self.__situacao = 0
         
+        if b == '': raise ValueError()
+        if e == datetime(1, 1, 1): raise ValueError()
+        if v == datetime(1, 1, 1): raise ValueError()
+        if p == datetime(1, 1, 1): raise ValueError()
+        if vb == 0.00: raise ValueError()
+        if vp == 0.00: raise ValueError()
+
+        def Pagar(self, vp):
+            if self.__valorBoleto == vp:
+                self.__situacao == 3
+            elif self.__valorBoleto > vp:
+                self.__situacao == 2
+            elif vp < 0:
+                self.__situacao == 1
+
